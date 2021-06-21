d26a10ca337a254a1d1c289ea41326e0321c7ce0
eugustavo <gustavo.ifto@gmail.com>
2021-06-20 22:31:46 -0300

    chore: Firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Button } from './components/Button';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Button text="Botão 01" />
+      <Button text="a" />
+      <Button text="b" />
+      <Button text="c" />
+      <Button text="d" />
+    </div>
   );
 }
 
